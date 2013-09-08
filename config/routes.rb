74d1e117a2c794469e5ca522e265a1dadd87dd6f
Fumihisa Yamanobe <addy0920@gmail.com>
2013-09-08 19:02:36 +0900

    routes set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Twitter::Application.routes.draw do
+
+  root 'tweets#index'
+  resources :tweets
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
