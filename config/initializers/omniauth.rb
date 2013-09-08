b10deb28667c518ffe1f6e499fe5a8ba2bbd872e
Fumihisa Yamanobe <addy0920@gmail.com>
2013-09-08 19:00:19 +0900

    controller added

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, "PNKEQjEnu3dKXzS1w7tVA", "RSwCNzsmYbV2uTouuFuH05Xhs33GdPUbPuda6AETf8"
+end
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,4 @@
+Twitter.configure do |config|
+  config.consumer_key = "PNKEQjEnu3dKXzS1w7tVA"
+  config.consumer_secret = "RSwCNzsmYbV2uTouuFuH05Xhs33GdPUbPuda6AETf8"
+end
